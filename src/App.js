97c5f78ae92b715bf411e737053b99c1fa91e4c1
hazedrops <hazedrops@hotmail.com>
2022-02-23 21:36:07 -0800

    Created listing form page etc.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import Navbar from './components/Navbar';
-import PrivateRoute from './components/PrivateRoute';
-import Explore from './pages/Explore';
-import ForgotPassword from './pages/ForgotPassword';
-import Offers from './pages/Offers';
-import Category from './pages/Category';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+import Category from './pages/Category'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import CreateListing from './pages/CreateListing'
 
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -17,15 +18,16 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={< Explore />} />
-          <Route path='/offers' element={< Offers />} />
-          <Route path='/category/:categoryName' element={< Category />} />
-          <Route path='/profile' element={< PrivateRoute />} >
-            <Route path='/profile' element={< Profile />} />
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
           </Route>
-          <Route path='/sign-in' element={< SignIn />} />
-          <Route path='/sign-up' element={< SignUp />} />
-          <Route path='/forgot-password' element={< ForgotPassword />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
 
         <Navbar />
@@ -33,7 +35,7 @@ function App() {
 
       <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
