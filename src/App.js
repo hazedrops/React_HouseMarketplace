9d55aa5d7bd5e0e0431255b4ce2cf3ba225aa834
hazedrops <hazedrops@hotmail.com>
2022-02-28 10:15:17 -0800

    Added listing detail page and implementing contact landlord page in progress

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -30,6 +31,8 @@ function App() {
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
+
         </Routes>
 
         <Navbar />
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -20,34 +20,80 @@ function Listing() {
       const docRef = doc(db, 'listings', params.listingId)
       const docSnap = await getDoc(docRef)
 
-      if(docSnap.exists()) {
-        console.log(docSnap.data());
+      if (docSnap.exists()) {
+        console.log(docSnap.data())
         setListing(docSnap.data())
         setLoading(false)
       }
     }
 
     fetchListing()
-
   }, [navigate, params.listingId])
+
+  if (loading) {
+    return <Spinner />
+  }
 
   return (
     <main>
       {/* Slider */}
-      <div className="shareIconDiv" onClick={() => {
-        // Sets the clipboard's contents to the current window url
-        navigator.clipboard.writeText(window.location.href)
-        
-        setShareLinkCopied(true)
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          // Sets the clipboard's contents to the current window url
+          navigator.clipboard.writeText(window.location.href)
 
-        setTimeout(() => {
-          setShareLinkCopied(false)
-        }, 2000)
-      }}>
-        <img src={shareIcon} alt="" />
+          setShareLinkCopied(true)
+
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt='' />
       </div>
 
       {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className='listingDetailsList'>
+          <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</li>
+          <li>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+        
+        {/* Map */}
+
+        {/* if the listing is not the user's listing */}
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
+            Contact Landlord
+          </Link>
+        )}
+      </div>
     </main>
   )
 }
